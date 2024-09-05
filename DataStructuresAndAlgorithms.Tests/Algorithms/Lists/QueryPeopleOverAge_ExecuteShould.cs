df67f84da2d70b7ec1cfef7fa00203224cc40d3b
Vinicius Rocha <viniciusroch65@gmail.com>
2024-09-05 19:44:39 -0300

    add linq query syntax exercise

---
+++ DataStructuresAndAlgorithms.Tests/Algorithms/Lists/QueryPeopleOverAge_ExecuteShould.cs
@@ -0,0 +1,29 @@
+﻿using DataStructuresAndAlgorithms.Algorithms.Lists;
+using DataStructuresAndAlgorithms.DataStructures;
+
+namespace DataStructuresAndAlgorithms.Tests.Algorithms.Lists;
+
+public class QueryPeopleOverAge_ExecuteShould
+{
+  [Fact]
+  public void Execute_ShouldReturnOrderedFilteredList()
+  {
+    // Arrange
+    var people = new List<Person> {
+      new(Name: "John", Age: 30, Country: "USA" ),
+      new(Name: "Jane", Age: 25, Country: "USA" ),
+      new(Name: "Doe", Age: 35 , Country: "USA" ),
+    };
+    var expected = new List<Person> {
+      new(Name: "Doe", Age: 35, Country: "USA" ),
+      new(Name: "John", Age: 30, Country: "USA" ),
+    };
+    var query = new QueryPeopleOverAge(people);
+
+    // Act
+    var result = query.Execute(29);
+
+    // Assert
+    Assert.Equal(expected, result);
+  }
+}
---
+++ DataStructuresAndAlgorithms/Algorithms/Lists/QueryPeopleOverAge.cs
@@ -0,0 +1,13 @@
+﻿using DataStructuresAndAlgorithms.DataStructures;
+
+namespace DataStructuresAndAlgorithms.Algorithms.Lists;
+
+public class QueryPeopleOverAge(List<Person> people)
+{
+  public List<Person> Execute(int age) => [..(
+    from person in people
+    where person.Age > age
+    orderby person.Name
+    select person
+  )];
+}
