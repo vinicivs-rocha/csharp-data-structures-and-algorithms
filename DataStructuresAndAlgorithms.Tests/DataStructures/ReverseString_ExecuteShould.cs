c41d4a6820049149e1eb93d2f1657507ac12f0bc
Vinicius <vinicivsdev@gmail.com>
2024-09-06 12:43:50 -0300

    add string reversing

---
+++ DataStructuresAndAlgorithms.Tests/DataStructures/ReverseString_ExecuteShould.cs
@@ -0,0 +1,20 @@
+﻿using DataStructuresAndAlgorithms.Algorithms.Stacks;
+
+namespace DataStructuresAndAlgorithms.Tests.DataStructures;
+
+public class ReverseString_ExecuteShould
+{
+  [Fact]
+  public void Execute_ShouldReverseString()
+  {
+    // Arrange
+    var input = "Hello, World!";
+    var expected = "!dlroW ,olleH";
+
+    // Act
+    var actual = ReverseString.Execute(input);
+
+    // Assert
+    Assert.Equal(expected, actual);
+  }
+}
---
+++ DataStructuresAndAlgorithms/Algorithms/Stacks/ReverseString.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace DataStructuresAndAlgorithms.Algorithms.Stacks;
+
+public class ReverseString
+{
+  public static string Execute(string input)
+  {
+    var stack = new Stack<char>();
+    foreach (var c in input)
+    {
+      stack.Push(c);
+    }
+
+    var reversed = new StringBuilder();
+    while (stack.Count > 0)
+    {
+      reversed.Append(stack.Pop());
+    }
+
+    return reversed.ToString();
+  }
+}
