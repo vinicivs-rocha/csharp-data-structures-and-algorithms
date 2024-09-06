c6a137d6b6a4cbd64b5a729ebd11c895a1cf73b1
Vinicius <vinicivsdev@gmail.com>
2024-09-06 12:40:02 -0300

    add doubly circular linked list

---
+++ DataStructuresAndAlgorithms.Tests/DataStructures/CircularLinkedList_PrevShould.cs
@@ -0,0 +1,25 @@
+﻿using DataStructuresAndAlgorithms.DataStructures;
+
+namespace DataStructuresAndAlgorithms.Tests.DataStructures;
+
+public class CircularLinkedList_PrevShould
+{
+  [Fact]
+  public void Prev_ShouldBeAbleToExecuteMoreThanListLengthTimes()
+  {
+    // Arrange
+    CircularLinkedList<int> circularLinkedList = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    var iterations = circularLinkedList.Count + 1;
+    var node = circularLinkedList.First;
+
+    // Act
+    for (int i = 0; i < iterations; i++)
+    {
+      node = node?.Prev();
+    }
+
+    // Assert
+    Assert.NotNull(node);
+    Assert.Equal(10, node.Value);
+  }
+}
--- DataStructuresAndAlgorithms/DataStructures/CircularLinkedListExtensions.cs
+++ DataStructuresAndAlgorithms/DataStructures/CircularLinkedListExtensions.cs
@@ -3,4 +3,5 @@
 public static class CircularLinkedListExtensions
 {
   public static LinkedListNode<TItem>? Next<TItem>(this LinkedListNode<TItem> node) => node?.List != null ? node.Next ?? node.List.First : null;
+  public static LinkedListNode<TItem>? Prev<TItem>(this LinkedListNode<TItem> node) => node?.List != null ? node.Previous ?? node.List.Last : null;
 }
