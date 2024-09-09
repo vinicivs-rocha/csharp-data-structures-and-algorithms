1d7f0c2a5d8ccf191e1915546cc5544adda88e9a
Vinicius <vinicivsdev@gmail.com>
2024-09-09 11:23:44 -0300

    add tower of hanoi solution

---
+++ DataStructuresAndAlgorithms.Tests/Algorithms/Stack/TowerOfHanoi_SolveShould.cs
@@ -0,0 +1,24 @@
+﻿using DataStructuresAndAlgorithms.Algorithms.Stacks;
+
+namespace DataStructuresAndAlgorithms.Tests.Algorithms.Stack;
+
+public class TowerOfHanoi_SolveShould
+{
+  [Fact]
+  public async void Solve() {
+    // Arrange
+    var disksCount = 3;
+    var from = new Stack<int>(Enumerable.Range(1, disksCount).Reverse());
+    TowerOfHanoi<int> towerOfHanoi = new(from, disksCount);
+
+    // Act
+    await towerOfHanoi.Solve();
+
+    // Assert
+    Assert.Empty(towerOfHanoi.From);
+    Assert.Equal(disksCount, towerOfHanoi.To.Count);
+    Assert.Empty(towerOfHanoi.Aux);
+    Assert.True(towerOfHanoi.To.SequenceEqual(Enumerable.Range(1, disksCount)), "To stack should have all disks in order");
+    Assert.True(towerOfHanoi.MovesCount == Math.Pow(2, disksCount) - 1, "Moves count should be 2^n - 1");
+  }
+}
---
+++ DataStructuresAndAlgorithms/Algorithms/Stacks/TowerOfHanoi.cs
@@ -0,0 +1,33 @@
+﻿namespace DataStructuresAndAlgorithms.Algorithms.Stacks;
+
+public class TowerOfHanoi<TItem>(Stack<TItem> from, int disksCount)
+{
+  public Stack<TItem> From { get; private set; } = from;
+
+  public Stack<TItem> To { get; private set; } = new();
+
+  public Stack<TItem> Aux { get; private set; } = new();
+
+  public int DisksCount { get; private set; } = disksCount;
+
+  public int MovesCount { get; private set; }
+
+  public event EventHandler<EventArgs>? MoveCompleted;
+
+  public async Task Solve() => await Move(DisksCount, From, To, Aux);
+
+  private async Task Move(int disksCount, Stack<TItem> from, Stack<TItem> to, Stack<TItem> aux)
+  {
+    if (disksCount == 0) { return; }
+
+    await Move(disksCount - 1, from, aux, to);
+
+    to.Push(from.Pop());
+
+    MovesCount++;
+
+    MoveCompleted?.Invoke(this, EventArgs.Empty);
+
+    await Move(disksCount - 1, aux, to, from);
+  }
+}
